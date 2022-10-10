ab0d806c444370df13288d34f02ec421ecc94fbb
shutkin.90 <shutkin.90@mail.ru>
2022-10-10 14:22:50 +0400

    setup QuestionVC

---
+++ AnimalQuiz/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  IntroductionViewController.swift
+//  AnimalQuiz
+//
+//  Created by Aleksandr on 09.10.2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {}
+
+
+
+
+}
+
---
+++ AnimalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ResultViewController.swift
+//  AnimalQuiz
+//
+//  Created by Aleksandr on 10.10.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
